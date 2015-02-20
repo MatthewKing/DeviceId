28c1c5ff66d99f195c19981ab30ee871b710f90c
Matthew King <mking@mking.io>
2015-02-20 16:28:10 +0800

    Added file token component.

---
+++ src/DeviceId/FileTokenDeviceIdComponent.cs
@@ -0,0 +1,46 @@
+﻿namespace DeviceId
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    public sealed class FileTokenDeviceIdComponent : IDeviceIdComponent
+    {
+        private readonly string name;
+        private readonly string path;
+
+        public string Name { get { return this.name; } }
+
+        public FileTokenDeviceIdComponent(string name, string path)
+        {
+            this.name = name;
+            this.path = path;
+        }
+
+        public string GetValue()
+        {
+            string value = Guid.NewGuid().ToString().ToUpper();
+
+            if (File.Exists(this.path))
+            {
+                try
+                {
+                    byte[] bytes = File.ReadAllBytes(this.path);
+                    value = Encoding.ASCII.GetString(bytes);
+                }
+                catch { }
+            }
+            else
+            {
+                try
+                {
+                    byte[] bytes = Encoding.ASCII.GetBytes(value);
+                    File.WriteAllBytes(this.path, bytes);
+                }
+                catch { }
+            }
+
+            return value;
+        }
+    }
+}
