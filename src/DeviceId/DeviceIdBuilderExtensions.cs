4d2849bea99be2628177dda1aeb9a41372474788
Matthew King <mking@mking.net>
2015-02-21 15:33:11 +0800

    Added extension methods for some of the most common components.

---
+++ src/DeviceId/DeviceIdBuilderExtensions.cs
@@ -0,0 +1,43 @@
+﻿namespace DeviceId
+{
+    using System;
+
+    public static class DeviceIdBuilderExtensions
+    {
+        public static DeviceIdBuilder AddUserName(this DeviceIdBuilder builder)
+        {
+            return builder.AddComponent(new DeviceIdComponent("UserName", Environment.UserName));
+        }
+
+        public static DeviceIdBuilder AddMachineName(this DeviceIdBuilder builder)
+        {
+            return builder.AddComponent(new DeviceIdComponent("MachineName", Environment.MachineName));
+        }
+
+        public static DeviceIdBuilder AddOSVersion(this DeviceIdBuilder builder)
+        {
+            return builder.AddComponent(new DeviceIdComponent("OSVersion", Environment.OSVersion.ToString()));
+        }
+
+        public static DeviceIdBuilder AddMacAddress(this DeviceIdBuilder builder)
+        {
+            return builder.AddComponent(new WmiDeviceIdComponent("MACAddress", "Win32_NetworkAdapterConfiguration", "MACAddress"));
+        }
+
+        public static DeviceIdBuilder AddProcessorId(this DeviceIdBuilder builder)
+        {
+            return builder.AddComponent(new WmiDeviceIdComponent("ProcessorId", "Win32_Processor", "ProcessorId"));
+        }
+
+        public static DeviceIdBuilder AddMotherboardSerialNumber(this DeviceIdBuilder builder)
+        {
+            return builder.AddComponent(new WmiDeviceIdComponent("MotherboardSerialNumber", "Win32_BaseBoard", "SerialNumber"));
+        }
+
+        public static DeviceIdBuilder AddFileToken(this DeviceIdBuilder builder, string path)
+        {
+            var name = String.Concat("FileToken", path.GetHashCode());
+            return builder.AddComponent(new FileTokenDeviceIdComponent(name, path));
+        }
+    }
+}
