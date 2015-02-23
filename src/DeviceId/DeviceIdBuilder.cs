68205e9c24cd66d0c0c68de3c936a000e38e5499
Matthew King <mking@mking.net>
2015-02-23 15:56:17 +0800

    XML documentation and minor code clean-up.

--- src/DeviceId/DeviceIdBuilder.cs
+++ src/DeviceId/DeviceIdBuilder.cs
@@ -6,29 +6,71 @@
     using System.Security.Cryptography;
     using System.Text;
 
+    /// <summary>
+    /// Provides a fluent interface for constructing unique device identifiers.
+    /// </summary>
     public sealed class DeviceIdBuilder
     {
+        /// <summary>
+        /// The comparer to use when comparing components for equality.
+        /// </summary>
+        private static IEqualityComparer<IDeviceIdComponent> comparer;
+
+        /// <summary>
+        /// A HashSet containing the components that will make up the device identifier.
+        /// </summary>
         private readonly HashSet<IDeviceIdComponent> components;
 
-        public DeviceIdBuilder()
+        /// <summary>
+        /// Initializes static members of the DeviceIdBuilder class.
+        /// </summary>
+        static DeviceIdBuilder()
         {
-            this.components = new HashSet<IDeviceIdComponent>(new DeviceIdComponentEqualityComparer());
+            comparer = new DeviceIdComponentEqualityComparer();
         }
 
+        /// <summary>
+        /// Initializes a new instance of the DeviceIdBuilder class.
+        /// </summary>
+        public DeviceIdBuilder()
+        {
+            this.components = new HashSet<IDeviceIdComponent>(comparer);
+        }
+
+        /// <summary>
+        /// Adds the specified component to this DeviceIdBuilder instance.
+        /// The component will be used to construct the device identifier.
+        /// </summary>
+        /// <param name="component">The component to be added.</param>
+        /// <returns>This DeviceIdBuilder instance.</returns>
         public DeviceIdBuilder AddComponent(IDeviceIdComponent component)
         {
             this.components.Add(component);
             return this;
         }
 
+        /// <summary>
+        /// Returns a string uniquely identifying this device, using the components specified
+        /// in this DeviceIdBuilder instance.
+        /// </summary>
+        /// <returns>A string uniquely identifying the device.</returns>
         public string GetDeviceId()
         {
             return this.GetDeviceId("SHA256");
         }
 
+        /// <summary>
+        /// Returns a string uniquely identifying this device, using the components specified
+        /// in this DeviceIdBuilder instance.
+        /// </summary>
+        /// <param name="hashName">The hash algorithm implementation to use.</param>
+        /// <returns>A string uniquely identifying the device.</returns>
         public string GetDeviceId(string hashName)
         {
-            if (this.components.Count == 0) return null;
+            if (this.components.Count == 0)
+            {
+                return null;
+            }
 
             IEnumerable<string> orderedValues = this.components
                 .OrderBy(o => o.Name)
--- src/DeviceId/DeviceIdComponentEqualityComparer.cs
+++ src/DeviceId/DeviceIdComponentEqualityComparer.cs
@@ -3,13 +3,27 @@
     using System;
     using System.Collections.Generic;
 
+    /// <summary>
+    /// Defines methods to support the comparison of IDeviceIdComponent instances for equality.
+    /// </summary>
     internal sealed class DeviceIdComponentEqualityComparer : IEqualityComparer<IDeviceIdComponent>
     {
+        /// <summary>
+        /// Determines whether the specified objects are equal.
+        /// </summary>
+        /// <param name="x">The first object to compare.</param>
+        /// <param name="y">The second object to compare.</param>
+        /// <returns>true if the specified objects are equal; otherwise, false.</returns>
         public bool Equals(IDeviceIdComponent x, IDeviceIdComponent y)
         {
             return StringComparer.OrdinalIgnoreCase.Equals(x.Name, y.Name);
         }
 
+        /// <summary>
+        /// Returns a hash code for the specified object.
+        /// </summary>
+        /// <param name="obj">The object for which a hash code is to be returned.</param>
+        /// <returns>A hash code for the specified object.</returns>
         public int GetHashCode(IDeviceIdComponent obj)
         {
             return StringComparer.OrdinalIgnoreCase.GetHashCode(obj.Name);
