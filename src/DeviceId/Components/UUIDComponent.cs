4ac4117ab91bead6893c05a7961df78aebb851d3
klyse <klaus.pruenster@gmail.com>
2019-05-21 16:38:21 +0200

    add UUID

---
+++ src/DeviceId/Components/UUIDComponent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Management;
+
+namespace DeviceId.Components
+{
+    /// <summary>
+    /// An implementation of <see cref="IDeviceIdComponent"/> that uses the UUID number.
+    /// </summary>
+    public class UUIDComponent : IDeviceIdComponent
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UUIDComponent"/> class.
+        /// </summary>
+        public UUIDComponent() { }
+
+        /// <summary>
+        /// Gets the name of the component.
+        /// </summary>
+        public string Name => "SystemUUID";
+
+        /// <summary>
+        /// Gets the component value.
+        /// </summary>
+        /// <returns>The component value.</returns>
+        public string GetValue()
+        {
+            var result = "";
+            var mc = new ManagementClass("Win32_ComputerSystemProduct");
+            var moc = mc.GetInstances();
+            foreach (var mo in moc)
+            {
+                //Only get the first one
+                if (result != "") continue;
+                try
+                {
+                    var moWmi = mo["UUID"];
+                    if (moWmi == null)
+                        continue;
+                    result = moWmi.ToString();
+                    break;
+                }
+                catch
+                {
+                    // ignored
+                }
+            }
+
+            return result;
+        }
+    }
+}
