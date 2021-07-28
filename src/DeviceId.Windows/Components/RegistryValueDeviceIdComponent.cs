1b0c66bde8e1e46bcc5ea62be688db6b8bf736c2
Matthew King <mking@mking.net>
2021-07-28 19:59:52 +0800

    Improve registry components.

--- src/DeviceId.Windows/Components/RegistryValueDeviceIdComponent.cs
+++ src/DeviceId.Windows/Components/RegistryValueDeviceIdComponent.cs
@@ -7,8 +7,20 @@ namespace DeviceId.Windows.Components
     /// </summary>
     public class RegistryValueDeviceIdComponent : IDeviceIdComponent
     {
+#if !NET35
         /// <summary>
-        /// The path of the registry key to look at.
+        /// The registry hive.
+        /// </summary>
+        private readonly RegistryHive _registryHive;
+
+        /// <summary>
+        /// The registry views.
+        /// </summary>
+        private readonly RegistryView _registryView;
+#endif
+
+        /// <summary>
+        /// The name of the registry key.
         /// </summary>
         private readonly string _keyName;
 
@@ -17,16 +29,33 @@ namespace DeviceId.Windows.Components
         /// </summary>
         private readonly string _valueName;
 
+#if NET35
         /// <summary>
         /// Initializes a new instance of the <see cref="RegistryValueDeviceIdComponent"/> class.
         /// </summary>
-        /// <param name="keyName">The full path of the registry key.</param>
+        /// <param name="keyName">The name of the registry key.</param>
         /// <param name="valueName">The name of the registry value.</param>
         public RegistryValueDeviceIdComponent(string keyName, string valueName)
         {
             _keyName = keyName;
             _valueName = valueName;
         }
+#else
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RegistryValueDeviceIdComponent"/> class.
+        /// </summary>
+        /// <param name="registryView">The registry view.</param>
+        /// <param name="registryHive">The registry hive.</param>
+        /// <param name="keyName">The name of the registry key.</param>
+        /// <param name="valueName">The name of the registry value.</param>
+        public RegistryValueDeviceIdComponent(RegistryView registryView, RegistryHive registryHive, string keyName, string valueName)
+        {
+            _registryHive = registryHive;
+            _registryView = registryView;
+            _keyName = keyName;
+            _valueName = valueName;
+        }
+#endif
 
         /// <summary>
         /// Gets the component value.
@@ -34,12 +63,25 @@ namespace DeviceId.Windows.Components
         /// <returns>The component value.</returns>
         public string GetValue()
         {
+#if NET35
             try
             {
-                var value = Registry.GetValue(_keyName, _valueName, null);
-                return value?.ToString();
+                return Registry.GetValue(_keyName, _valueName, null)?.ToString();
             }
             catch { }
+#else
+            try
+            {
+                using var registry = RegistryKey.OpenBaseKey(_registryHive, _registryView);
+                using var subKey = registry.OpenSubKey(_keyName);
+                if (subKey != null)
+                {
+                    var value = subKey.GetValue(_valueName);
+                    return value?.ToString();
+                }
+            }
+            catch { }
+#endif
 
             return null;
         }
--- src/DeviceId.Windows/WindowsDeviceIdBuilderExtensions.cs
+++ src/DeviceId.Windows/WindowsDeviceIdBuilderExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using DeviceId.Windows.Components;
+#if !NET35
+using Microsoft.Win32;
+#endif
 
 namespace DeviceId.Windows
 {
@@ -7,6 +10,7 @@ namespace DeviceId.Windows
     /// </summary>
     public static class WindowsDeviceIdBuilderExtensions
     {
+#if NET35
         /// <summary>
         /// Adds a registry value to the device identifier.
         /// </summary>
@@ -19,6 +23,21 @@ namespace DeviceId.Windows
         {
             return builder.AddComponent(componentName, new RegistryValueDeviceIdComponent(registryKeyName, registryValueName));
         }
+#else
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RegistryValueDeviceIdComponent"/> class.
+        /// </summary>
+        /// <param name="builder">The <see cref="WindowsDeviceIdBuilder"/> to add the component to.</param>
+        /// <param name="componentName">The name of the component.</param>
+        /// <param name="registryView">The registry view.</param>
+        /// <param name="registryHive">The registry hive.</param>
+        /// <param name="registryKeyName">The name of the registry key.</param>
+        /// <param name="registryValueName">The name of the registry value.</param>
+        public static WindowsDeviceIdBuilder AddRegistryValue(this WindowsDeviceIdBuilder builder, string componentName, RegistryView registryView, RegistryHive registryHive, string registryKeyName, string registryValueName)
+        {
+            return builder.AddComponent(componentName, new RegistryValueDeviceIdComponent(registryView, registryHive, registryKeyName, registryValueName));
+        }
+#endif
 
         /// <summary>
         /// Adds the Machine GUID (from HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Cryptography\MachineGuid) to the device identifier.
@@ -27,10 +46,19 @@ namespace DeviceId.Windows
         /// <returns>The <see cref="WindowsDeviceIdBuilder"/> instance.</returns>
         public static WindowsDeviceIdBuilder AddMachineGuid(this WindowsDeviceIdBuilder builder)
         {
+#if NET35
             return AddRegistryValue(builder,
                 "MachineGuid",
                 @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Cryptography",
                 "MachineGuid");
+#else
+            return AddRegistryValue(builder,
+                "MachineGuid",
+                RegistryView.Registry64,
+                RegistryHive.LocalMachine,
+                @"SOFTWARE\Microsoft\Cryptography",
+                "MachineGuid");
+#endif
         }
     }
 }
