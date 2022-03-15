c8c45af07a7da6029f13213c4f47fa17cb6b3fbb
Alexander Schuetz <fenvariel@googlemail.com>
2022-03-15 13:29:15 +0100

    Update DockerContainerIdComponentTests.cs

--- test/DeviceId.Tests/Components/DockerContainerIdComponentTests.cs
+++ test/DeviceId.Tests/Components/DockerContainerIdComponentTests.cs
@@ -12,6 +12,7 @@ public class DockerContainerIdComponentTests
     [InlineData(null, null)]
     [InlineData("Linux_4.4.txt", "cde7c2bab394630a42d73dc610b9c57415dced996106665d427f6d0566594411")]
     [InlineData("Linux_4.8-4.13.txt", "afe96d48db6d2c19585572f986fc310c92421a3dac28310e847566fb82166013")]
+    [InlineData("linux_nodocker.txt", null)]
     [Theory]
     public void TestGetValue(string file, string expected)
     {
