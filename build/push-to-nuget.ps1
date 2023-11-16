b9d0e33a73735f683290dfe7d0a02feef0fd095d
Matthew King <mking@mking.net>
2023-11-16 19:33:30 +0800

    Update NuGet push script.

--- build/push-to-nuget.ps1
+++ build/push-to-nuget.ps1
@@ -1,5 +1,7 @@
+$apiKey = Read-Host -Prompt "Enter your nuget.org API key"
+
 $root = Resolve-Path (Join-Path $PSScriptRoot "..")
 $output = "$root/artifacts"
 foreach ($package in Get-ChildItem -Path $output -Filter "*.nupkg") {
-    nuget push $package.FullName -Source https://api.nuget.org/v3/index.json
+    dotnet nuget push $package.FullName --source https://api.nuget.org/v3/index.json --api-key $apiKey
 }
